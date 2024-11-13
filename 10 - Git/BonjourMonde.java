eca3fd5c20bbf272cbab8bf6854b66991809de86
Omar Lounis <nopeash08@gmail.com>
2024-11-13 03:01:18 +0000

    "." est mieux que "!"

--- 10 - Git/BonjourMonde.java
+++ 10 - Git/BonjourMonde.java
@@ -5,7 +5,7 @@ public class BonjourMonde {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("bonjour monde!");
+        System.out.println("bonjour monde.");
 
         sc.close();
     }
